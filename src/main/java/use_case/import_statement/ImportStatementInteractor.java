77ab474aec71bf360801756ddf3d816e71bac41a
Parsa Nabifar <parsanabifar1385@gmail.com>
2025-11-24 11:56:30 -0500

    Saving work

--- src/main/java/use_case/import_statement/ImportStatementInteractor.java
+++ src/main/java/use_case/import_statement/ImportStatementInteractor.java
@@ -1,6 +1,17 @@
 package use_case.import_statement;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.io.File;
+import java.io.FileReader;
 import java.time.YearMonth;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 /**
  * The Import Bank Statement Interactor.
@@ -18,6 +29,54 @@ public class ImportStatementInteractor implements ImportStatementInputBoundary {
 
     @Override
     public void execute(ImportStatementInputData inputData) {
+       File file = new File(inputData.getFilePath());
+       if (!file.exists()){
+           presenter.prepareFailView("Import unsuccessful: file does not exist");
+       }
+
+       JsonArray array;
+        try {
+            array = readArrayFromFile(inputData.getFilePath());
+        } catch (Exception e) {
+            presenter.prepareFailView("Import unsuccessful: unsupported file");
+            return;
+        }
+
+        List<JsonObject> categorized = new ArrayList<>();
+        List<JsonObject> uncategorized = new ArrayList<>();
+
+        try {
+            separateTransactions(transactions, categorized, uncategorized);
+        } catch (Exception e) {
+            presenter.prepareFailView("Import unsuccessful: unsupported file");
+            return;
+        }
+
+        try {
+            addTransactions(categorized);
+        } catch (Exception e) {
+            presenter.prepareFailView("failed to save categorized transactions");
+            return;
+        }
+
+
 
     }
+
+
+
+    private JsonArray readArrayFromFile(String filePath) throws Exception {
+        try (FileReader reader = new FileReader(filePath)) {
+
+            JsonElement element = JsonParser.parseReader(reader);
+
+            if (!element.isJsonArray()) {
+                throw new Exception("File does not contain a JSON array");
+            }
+
+            return element.getAsJsonArray();
+        }
+    }
+
+
 }
